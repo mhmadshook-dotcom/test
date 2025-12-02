7da2daa8cccc3b35d1fc6f855210a443bacb83dc
mhmadshook-dotcom <mhmad.shook@gmail.com>
2025-12-03 01:51:43 +0200

    Updates

--- src/Component/index.js
+++
@@ -1,9 +0,0 @@
-export {default as Container}  from './Container/Container';
-export {default as NavItem}    from './Navbar/NavItem';
-export {NavItemDropDown as NavItemDropDown}    from './Navbar/NavItem';
-export {default as Card}    from './Card/Card';
-export {default as SectionHeader}    from './SectionHeader/SectionHeader';
-export {default as SectionWrapper}    from './SectionWrapper/SectionWrapper';
-export {default as GamingLibraryCard}    from './GamingLibraryCard/GamingLibraryCard';
-export {default as PrimaryButton}    from './Buttons/Buttons';
-export {SecondaryButton as SecondaryButton}    from './Buttons/Buttons';
